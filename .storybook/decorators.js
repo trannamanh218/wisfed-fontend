418af54c7c154a2322b4e7dccd832b3ee9cdfda6
Tran Thuy <thuytt0295@gmail.com>
2022-03-15 10:55:07 +0700

    write stories book register page, login page,forget-password page

--- .storybook/decorators.js
+++ .storybook/decorators.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import App from '../src/App';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import rootReducer from '../src/reducers/redux-utils';
 
 export const withRouter = (StoryFn, { parameters: { deeplink } }) => {
-	// if there's no deeplink config, just return the story in a Router
+
 	if (!deeplink) {
 		return (
 			<BrowserRouter>
@@ -12,19 +15,29 @@ export const withRouter = (StoryFn, { parameters: { deeplink } }) => {
 		);
 	}
 
-	const { path, route } = deeplink;
-
 	return (
-		// <MemoryRouter initialEntries={[encodeURI(route)]}>
-		//   <App>
-		// 	<Route path={path} element={<StoryFn />} />
-		//   </App>
-		// </MemoryRouter>
 		<BrowserRouter>
 			<App />
 		</BrowserRouter>
 	);
 };
 
+export const withStore = (StoryFn, { parameters }) => {
+	// Creates a store by merging optional custom initialState
+	const store = configureStore({
+		middleware: getDefaultMiddleware =>
+			getDefaultMiddleware({
+				serializableCheck: false,
+			}),
+		reducer: rootReducer,
+		preloadedState: parameters.store?.initialState, // if undefined, just use default state from reducers
+	});
+	return (
+		<Provider store={store}>
+			<StoryFn />
+		</Provider>
+	);
+};
+
 // ordered from innermost to outermost, be careful with the order!
-export const globalDecorators = [withRouter];
+export const globalDecorators = [withRouter, withStore];
