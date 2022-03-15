044575c627e167d5bfc293287a007c33edf654e3
Tran Thuy <thuytt0295@gmail.com>
2022-03-15 15:24:06 +0700

    add env file to git ignore

--- src/stories/pages/CategoryPageDetail.stories.js
+++ src/stories/pages/CategoryPageDetail.stories.js
@@ -3,14 +3,14 @@ import CategoryDetail from 'pages/category-detail';
 export default {
 	title: 'Pages/Category Page Detail',
 	component: CategoryDetail,
+	parameters: {
+		deeplink: {
+			path: '/category/detail/:id',
+			route: '/category/detail/12',
+		},
+	},
 };
 
 const Template = () => <CategoryDetail />;
 
 export const Default = Template.bind({});
-Default.parameters = {
-	deeplink: {
-		path: '/category/detail/:id',
-		route: '/category/detail/12',
-	},
-};
