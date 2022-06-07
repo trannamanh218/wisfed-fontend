d30cb7d9c0cea7061e4ea31487ce376577963251
Shadow <hoangquanx98x@gmail.com>
2022-06-07 14:21:49 +0700

    add friend folow group

--- src/pages/group-page/popup-group/MainGroupComponet/component/MemberGroup.jsx
+++ src/pages/group-page/popup-group/MainGroupComponet/component/MemberGroup.jsx
@@ -2,10 +2,32 @@
 import React, { useEffect, useState } from 'react';
 import './memmber-group.scss';
 import PropTypes from 'prop-types';
+import { makeFriendRequest, addFollower, unFollower, unFriendRequest } from 'reducers/redux-utils/user';
+import { useDispatch } from 'react-redux';
+import { NotificationError } from 'helpers/Error';
+import { useParams } from 'react-router-dom';
+import { getMember } from 'reducers/redux-utils/group';
 
-function MemberGroup({ memberGroups }) {
+function MemberGroup() {
 	const [listFriend, setListFriend] = useState([]);
 	const [listFolow, setListFolow] = useState([]);
+	const dispatch = useDispatch();
+	const { id = '' } = useParams();
+	const [memberGroups, setMemberGroups] = useState([]);
+	const [isCallApi, setIsCallApi] = useState(false);
+
+	const getListMember = async () => {
+		try {
+			const actionGetList = await dispatch(getMember(id)).unwrap();
+			setMemberGroups(actionGetList);
+		} catch (err) {
+			NotificationError(err);
+		}
+	};
+
+	useEffect(() => {
+		getListMember();
+	}, [isCallApi]);
 
 	useEffect(() => {
 		const newListFolow = memberGroups.filter(item => item.isFollowMe === true);
@@ -13,6 +35,47 @@ function MemberGroup({ memberGroups }) {
 		const newListFriend = memberGroups.filter(item => item.relation === 'friend');
 		setListFriend(newListFriend);
 	}, [memberGroups]);
+
+	const handleAddFriend = item => {
+		try {
+			const param = {
+				userId: item.id,
+			};
+			dispatch(makeFriendRequest(param)).unwrap();
+			setIsCallApi(!isCallApi);
+		} catch (err) {
+			NotificationError(err);
+		}
+	};
+	const handleUnFriend = item => {
+		// setModalUnfriends(false);
+		try {
+			dispatch(unFriendRequest(item.id)).unwrap();
+			setIsCallApi(!isCallApi);
+		} catch (err) {
+			NotificationError(err);
+		}
+	};
+
+	const handleFollow = item => {
+		try {
+			const param = {
+				data: { userId: item.id },
+			};
+			dispatch(addFollower(param)).unwrap();
+			setIsCallApi(!isCallApi);
+		} catch (err) {
+			NotificationError(err);
+		}
+	};
+	const handleUnFollow = item => {
+		try {
+			dispatch(unFollower(item.id)).unwrap();
+			setIsCallApi(!isCallApi);
+		} catch (err) {
+			NotificationError(err);
+		}
+	};
 
 	return (
 		<div className='member-group__container'>
@@ -56,15 +119,46 @@ function MemberGroup({ memberGroups }) {
 										</div>
 									</div>
 
-									<div style={{ display: 'flex' }}>
-										<button className='member-item__btn btn-folow'>Theo dõi</button>
-										<button className='member-item__btn bnt-add-friend'>+ Thêm bạn</button>
-										{/* {item.isAdmin && (
-											<button className='more-icon-btn-group'>
-												<MoreIcon />
-											</button>
-										)} */}
-									</div>
+									{item.relation !== 'isMe' && (
+										<div style={{ display: 'flex' }}>
+											{item.isFollow === true ? (
+												<button
+													className='member-item__btn btn-folow'
+													onClick={() => handleUnFollow(item)}
+												>
+													Hủy theo dõi
+												</button>
+											) : (
+												<button
+													className='member-item__btn btn-folow'
+													onClick={() => handleFollow(item)}
+												>
+													Theo dõi
+												</button>
+											)}
+											{item.relation === 'friend' && (
+												<button
+													className='member-item__btn bnt-add-friend'
+													onClick={() => handleUnFriend()}
+												>
+													- hủy kết bạn
+												</button>
+											)}
+											{item.relation === 'pending' && (
+												<button className='member-item__btn bnt-add-friend'>
+													Đã gửi lời mời
+												</button>
+											)}
+											{item.relation === 'unknown' && (
+												<button className='member-item__btn bnt-add-friend'>+ Thêm bạn</button>
+											)}
+											{/* {item.isAdmin && (x
+													<button className='more-icon-btn-group'>
+														<MoreIcon />
+													</button>
+												)} */}
+										</div>
+									)}
 								</div>
 							)}
 						</>
@@ -111,15 +205,51 @@ function MemberGroup({ memberGroups }) {
 											</div>
 										</div>
 
-										<div>
-											<button className='member-item__btn btn-folow'>Theo dõi</button>
-											<button className='member-item__btn bnt-add-friend'>+ Thêm bạn</button>
-											{/* {userInfo?.id === adminGroup?.id && (
-												<button className='more-icon-btn-group'>
-													<MoreIcon />
-												</button>
-											)} */}
-										</div>
+										{item.relation !== 'isMe' && (
+											<div style={{ display: 'flex' }}>
+												{item.isFollow === true ? (
+													<button
+														className='member-item__btn btn-folow'
+														onClick={() => handleUnFollow(item)}
+													>
+														Hủy theo dõi
+													</button>
+												) : (
+													<button
+														className='member-item__btn btn-folow'
+														onClick={() => handleFollow(item)}
+													>
+														Theo dõi
+													</button>
+												)}
+												{item.relation === 'friend' && (
+													<button
+														className='member-item__btn bnt-add-friend'
+														onClick={() => handleUnFriend(item)}
+													>
+														- hủy kết bạn
+													</button>
+												)}
+												{item.relation === 'pending' && (
+													<button className='member-item__btn bnt-add-friend'>
+														Đã gửi lời mời
+													</button>
+												)}
+												{item.relation === 'unknown' && (
+													<button
+														className='member-item__btn bnt-add-friend'
+														onClick={() => handleAddFriend(item)}
+													>
+														+ Thêm bạn
+													</button>
+												)}
+												{/* {item.isAdmin && (x
+													<button className='more-icon-btn-group'>
+														<MoreIcon />
+													</button>
+												)} */}
+											</div>
+										)}
 									</div>
 								</>
 							);
@@ -164,16 +294,51 @@ function MemberGroup({ memberGroups }) {
 												)}
 											</div>
 										</div>
-
-										<div>
-											<button className='member-item__btn btn-folow'>Theo dõi</button>
-											<button className='member-item__btn bnt-add-friend'>+ Thêm bạn</button>
-											{/* {userInfo?.id === adminGroup?.id && (
-												<button className='more-icon-btn-group'>
-													<MoreIcon />
-												</button>
-											)} */}
-										</div>
+										{item.relation !== 'isMe' && (
+											<div style={{ display: 'flex' }}>
+												{item.isFollow === true ? (
+													<button
+														className='member-item__btn btn-folow'
+														onClick={() => handleUnFollow(item)}
+													>
+														Hủy theo dõi
+													</button>
+												) : (
+													<button
+														className='member-item__btn btn-folow'
+														onClick={() => handleFollow(item)}
+													>
+														Theo dõi
+													</button>
+												)}
+												{item.relation === 'friend' && (
+													<button
+														className='member-item__btn bnt-add-friend'
+														onClick={() => handleUnFriend(item)}
+													>
+														- hủy kết bạn
+													</button>
+												)}
+												{item.relation === 'pending' && (
+													<button className='member-item__btn bnt-add-friend'>
+														Đã gửi lời mời
+													</button>
+												)}
+												{item.relation === 'unknown' && (
+													<button
+														className='member-item__btn bnt-add-friend'
+														onClick={() => handleAddFriend(item)}
+													>
+														+ Thêm bạn
+													</button>
+												)}
+												{/* {item.isAdmin && (x
+													<button className='more-icon-btn-group'>
+														<MoreIcon />
+													</button>
+												)} */}
+											</div>
+										)}
 									</div>
 								</>
 							);
@@ -221,15 +386,50 @@ function MemberGroup({ memberGroups }) {
 									</div>
 								</div>
 
-								<div>
-									<button className='member-item__btn btn-folow'>Theo dõi</button>
-									<button className='member-item__btn bnt-add-friend'>+ Thêm bạn</button>
-									{/* {userInfo?.id === adminGroup?.id && (
-										<button className='more-icon-btn-group'>
-											<MoreIcon />
-										</button>
-									)} */}
-								</div>
+								{item.relation !== 'isMe' && (
+									<div style={{ display: 'flex' }}>
+										{item.isFollow === true ? (
+											<button
+												className='member-item__btn btn-folow'
+												onClick={() => handleUnFollow(item)}
+											>
+												Hủy theo dõi
+											</button>
+										) : (
+											<button
+												className='member-item__btn btn-folow'
+												onClick={() => handleFollow(item)}
+											>
+												Theo dõi
+											</button>
+										)}
+
+										{item.relation === 'friend' && (
+											<button
+												className='member-item__btn bnt-add-friend'
+												onClick={() => handleUnFriend(item)}
+											>
+												- hủy kết bạn
+											</button>
+										)}
+										{item.relation === 'pending' && (
+											<button className='member-item__btn bnt-add-friend'>Đã gửi lời mời</button>
+										)}
+										{item.relation === 'unknown' && (
+											<button
+												className='member-item__btn bnt-add-friend'
+												onClick={() => handleAddFriend(item)}
+											>
+												+ Thêm bạn
+											</button>
+										)}
+										{/* {item.isAdmin && (x
+													<button className='more-icon-btn-group'>
+														<MoreIcon />
+													</button>
+												)} */}
+									</div>
+								)}
 							</div>
 						</>
 					);
