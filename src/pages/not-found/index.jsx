f71cf1798aab7c95d1121e36adb5ee4f1ba65afa
namvt3 <vunamdhhl@gmail.com>
2022-10-31 11:05:22 +0700

    sửa lỗi nháy nháy khi render NotFound

--- src/pages/not-found/index.jsx
+++ src/pages/not-found/index.jsx
@@ -3,18 +3,26 @@ import React from 'react';
 import notFoundImage from 'assets/images/404.png';
 import './not-found.scss';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 function NotFound() {
+	const [imgLoaded, setImgLoaded] = useState(false);
+
 	return (
 		<NormalContainer>
-			<div className='not-found'>
-				<h4 className='not-found__title'>Opps!... Đã có lỗi xảy ra</h4>
-				<p>Vui lòng thử truy cập lại</p>
-				<img src={notFoundImage} alt='not found' />
-				<Link className='btn btn-primary not-found__btn' to='/'>
-					Về trang chủ
-				</Link>
-			</div>
+			<img src={notFoundImage} alt='not found' onLoad={() => setImgLoaded(true)} style={{ display: 'none' }} />
+			{imgLoaded ? (
+				<div className='not-found'>
+					<h4 className='not-found__title'>Opps!... Đã có lỗi xảy ra</h4>
+					<p>Vui lòng thử truy cập lại</p>
+					<img src={notFoundImage} alt='not found' />
+					<Link className='btn btn-primary not-found__btn' to='/'>
+						Về trang chủ
+					</Link>
+				</div>
+			) : (
+				<></>
+			)}
 		</NormalContainer>
 	);
 }
