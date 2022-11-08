4a3c0a596838b94209726622290a29424ef2420f
namvt3 <vunamdhhl@gmail.com>
2022-11-08 10:58:27 +0700

    viết code gọn lại

--- src/pages/book-detail/book-review/components/ReviewTab.jsx
+++ src/pages/book-detail/book-review/components/ReviewTab.jsx
@@ -117,11 +117,8 @@ const ReviewTab = ({ currentTab }) => {
 			let response;
 			if (currentOption.value === 'allReviews') {
 				response = await dispatch(getReviewsBook({ bookId, params })).unwrap();
-				if (response.count) {
-					dispatch(updateCurrentBookReviewsNumber(response.count));
-				} else {
-					dispatch(updateCurrentBookReviewsNumber(0));
-				}
+				// Lấy số đếm tổng review
+				dispatch(updateCurrentBookReviewsNumber(response.count || 0));
 			} else if (currentOption.value === 'friendReviews') {
 				response = await dispatch(getReviewsBookByFriends({ bookId, params })).unwrap();
 			} else {
