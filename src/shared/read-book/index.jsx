f918ba326bbfdc3c4903f9fc7632d17cf8b6911e
namvt3 <vunamdhhl@gmail.com>
2023-01-31 16:26:42 +0700

    thêm navigate sang chi tiết sách ở màn /reading-summary/:userId khi bấm vào sách

--- src/shared/read-book/index.jsx
+++ src/shared/read-book/index.jsx
@@ -2,8 +2,16 @@ import PropTypes from 'prop-types';
 import bookImage from 'assets/images/default-book.png';
 import ReactRating from 'shared/react-rating';
 import './read-book.scss';
+import { useNavigate } from 'react-router-dom';
+import RouteLink from 'helpers/RouteLink';
 
 const ReadBook = ({ items }) => {
+	const navigate = useNavigate();
+
+	const goToBookDetail = () => {
+		navigate(RouteLink.bookDetail(items.book.id, items.book.name));
+	};
+
 	return (
 		<>
 			<div className='read-book'>
@@ -12,9 +20,14 @@ const ReadBook = ({ items }) => {
 						src={items.book.frontBookCover || items.book.images[0] || bookImage}
 						alt='image-book-cover'
 						onError={e => e.target.setAttribute('src', `${bookImage}`)}
+						onClick={goToBookDetail}
 					/>
 				</div>
-				<h5 className='read-book__name' dangerouslySetInnerHTML={{ __html: items.book.name }}></h5>
+				<h5
+					className='read-book__name'
+					dangerouslySetInnerHTML={{ __html: items.book.name }}
+					onClick={goToBookDetail}
+				/>
 				<ReactRating initialRating={items.book.avgRating || 4} readonly />
 			</div>
 		</>
